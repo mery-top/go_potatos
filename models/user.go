f3b6b78537a9b75bbcb6d8a952d57988b66449ac
Meerthika <meerthikasrrajendrans@gmail.com>
2025-05-03 10:12:58 +0530

    Add GetUserByEmail

---
+++ models/user.go
@@ -0,0 +1,23 @@
+package models
+import(
+	"go_potatos/database"
+	"database/sql"
+	"error"
+)
+
+type User struct{
+	ID int
+	Email string
+	Password string
+}
+
+func GetUserByEmail(email string) (*User, error){
+	row:= database.db.QueryRow("select id, email, password from users where email=$1", email)
+	user:=&User{}
+	err:= row.Scan(&user.ID, &user.Email, &user.Password)
+	if err!=nil{
+		return nil, errors.New("Sql Has No Rows")
+	}
+	return user, err
+}
+
